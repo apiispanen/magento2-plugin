a2b12a0a00f1d466246313c7a5e74924638c7b1d
Andrew Piispanen <apiispanen1@babson.edu>
2023-08-16 14:26:41 -0400

    more legit?

---
+++ Block/Adminhtml/Settings.php
@@ -0,0 +1,10 @@
+<?php 
+// app/code/Dolphin/TutorialPlugin/Block/Adminhtml/Settings.php
+namespace Dolphin\TutorialPlugin\Block\Adminhtml;
+
+use Magento\Backend\Block\Template;
+
+class Settings extends Template
+{
+    // You can add your logic and methods here
+}

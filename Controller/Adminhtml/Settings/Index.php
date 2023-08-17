6e733c932943e4c92c930f1bb84ab548fbce5b08
Andrew Piispanen <apiispanen1@babson.edu>
2023-08-17 13:40:21 -0400

    index controller

--- Controller/Adminhtml/Settings/Index.php
+++ Controller/Adminhtml/Settings/Index.php
@@ -1,22 +1,36 @@
-<?php 
+<?php
 namespace Dolphin\TutorialPlugin\Controller\Adminhtml\Settings;
 
 use Magento\Backend\App\Action;
-use Magento\Framework\App\Action\HttpGetActionInterface; // Import this interface
+use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Data\Form\FormKey\Validator as FormKeyValidator; // Import FormKeyValidator
+use Psr\Log\LoggerInterface; // Import LoggerInterface
 
-class Index extends Action implements HttpGetActionInterface // Implement this interface
+class Index extends Action implements HttpGetActionInterface
 {
     protected $resultPageFactory;
+    protected $_formKeyValidator; // Define FormKeyValidator
+    protected $_logger; // Define Logger
 
-    public function __construct(Context $context, PageFactory $resultPageFactory)
-    {
+    public function __construct(
+        Action\Context $context,
+        PageFactory $resultPageFactory,
+        FormKeyValidator $formKeyValidator, // Inject FormKeyValidator
+        LoggerInterface $logger // Inject Logger
+    ) {
         $this->resultPageFactory = $resultPageFactory;
+        $this->_formKeyValidator = $formKeyValidator; // Initialize FormKeyValidator
+        $this->_logger = $logger; // Initialize Logger
         parent::__construct($context);
     }
 
     public function execute()
     {
+        // Log the Form Keys
+        $this->_logger->info('Form Key from Post: ' . $this->getRequest()->getParam('form_key'));
+        $this->_logger->info('Form Key from Session: ' . $this->_formKeyValidator->getFormKey());
+
         $resultPage = $this->resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->prepend(__('Tutorial Plugin Settings'));
         return $resultPage;

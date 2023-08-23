b64e5bb88b424e8769d4f5017884bcd27e5d6862
Andrew Piispanen <apiispanen1@babson.edu>
2023-08-23 13:31:05 -0400

    admin panel

---
+++ Controller/Adminhtml/Settings/HelloWorld/Index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Dolphin\TutorialPlugin\Controller\Adminhtml\HelloWorld;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Dolphin_TutorialPlugin::greetings_helloworld';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/TutorialPlugin_helloworld_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Hello World'));
+
+        return $resultPage;
+    }
+}

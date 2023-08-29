e911a369fceb7628779448567882232bb56e8e63
Andrew Piispanen <apiispanen1@babson.edu>
2023-08-29 12:24:56 -0400

    test

--- ExampleAdminNewPage/Controller/Adminhtml/HelloWorld/chatbot.php
+++ ExampleAdminNewPage/Controller/Adminhtml/HelloWorld/chatbot.php
@@ -2,6 +2,7 @@
     // With PHP, make an api request to the chatbot on click of the submit button:
 
     namespace Dolphin\ExampleAdminNewPage\Controller\Adminhtml\Helloworld;
+    require 'vendor/autoload.php';
 
     use Magento\Backend\App\Action;
     use Magento\Backend\App\Action\Context;
@@ -23,39 +24,38 @@
         {
             $result = $this->resultJsonFactory->create();
 
-    require 'vendor/autoload.php';
-    
-    // repo is https://github.com/openai-php/client
-    // echo "Hello World!";
-    $yourApiKey = getenv('OPENAI_API_KEY');
-    // echo $yourApiKey;
+            
+            // repo is https://github.com/openai-php/client
+            // echo "Hello World!";
+            $yourApiKey = getenv('OPENAI_API_KEY');
+            // echo $yourApiKey;
 
-    $client = OpenAI::client($yourApiKey);
-    $response = $client->chat()->create([
-        'model' => 'gpt-3.5-turbo',
-        'messages' => [
-            ['role' => 'user', 'content' => $_POST['text']],
-        ],
-    ]);
-    
-    $response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
-    $response->object; // 'chat.completion'
-    $response->created; // 1677701073
-    $response->model; // 'gpt-3.5-turbo-0301'
-    
-    foreach ($response->choices as $result) {
-        $result->index; // 0
-        $result->message->role; // 'assistant'
-        $result->message->content; // '\n\nHello there! How can I assist you today?'
-        $result->finishReason; // 'stop'
-    }
-    
-    $response->usage->promptTokens; // 9,
-    $response->usage->completionTokens; // 12,
-    $response->usage->totalTokens; // 21
-    
-    // return the response
-    echo json_encode($response);
-    return $result->setData($response);
+            $client = OpenAI::client($yourApiKey);
+            $response = $client->chat()->create([
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    ['role' => 'user', 'content' => $_POST['text']],
+                ],
+            ]);
+            
+            $response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
+            $response->object; // 'chat.completion'
+            $response->created; // 1677701073
+            $response->model; // 'gpt-3.5-turbo-0301'
+            
+            foreach ($response->choices as $result) {
+                $result->index; // 0
+                $result->message->role; // 'assistant'
+                $result->message->content; // '\n\nHello there! How can I assist you today?'
+                $result->finishReason; // 'stop'
+            }
+            
+            $response->usage->promptTokens; // 9,
+            $response->usage->completionTokens; // 12,
+            $response->usage->totalTokens; // 21
+            
+            // return the response
+            echo json_encode($response);
+            return $result->setData($response);
 }
 }
